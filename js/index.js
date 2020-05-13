57c34d7527bad96ed0f3af895e1523e208d9a9ac
Kagwi Andrew <kagwiandrew@gmail.com>
2020-05-13 13:58:07 +0300

    added js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,24 @@
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
 
+function removeBorder(){
+    tabItems.forEach(item=>item.classList.remove('tab-border'));
+
+}
 function selectItem(e){
+
+    removeBorder();
+    removeShow();
 this.classList.add('tab-border');
+console.log(this.id)
+const tabContentItem =document.querySelector(`#${this.id}-content`);
+
+tabContentItem.classList.add('show');
 
 }
 
+function removeShow(){
+    tabContentItems.forEach(item=>item.classList.remove('show'));
+
+}
 tabItems.forEach(item=>item.addEventListener('click',selectItem));
